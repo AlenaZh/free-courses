2f813b5a3016b9320217d7931085884a05067f83
Alena Zhilina <cont116@gmail.com>
2018-05-29 10:55:40 +0300

    change inputs task1

--- task1.js
+++ task1.js
@@ -1,6 +1,6 @@
 var inString = process.argv[1];
 var clearString = inString.replace(/[^A-Za-zА-Яа-яЁё]/g, "");
-var let =clearString.split('').reverse();
-var reverseClearString = let.join('');
+var lett =clearString.split('').reverse();
+var reverseClearString = lett.join('');
 
 process.stdout.write(String(clearString.toUpperCase() === reverseClearString.toUpperCase()?'YES':'NO'));
