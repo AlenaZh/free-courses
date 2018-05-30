148bb630a94aa5c0bd97be83a942730190bc0cac
Alena Zhilina <cont116@gmail.com>
2018-05-30 13:35:47 +0300

    task1.3

--- task1.js
+++ task1.js
@@ -1,6 +1,19 @@
-var inString = process.argv[1];
-var clearString = inString.replace(/[^A-Za-zА-Яа-яЁё]/g, "");
-var lett =clearString.split('').reverse();
-var reverseClearString = lett.join('');
+var inString = '';
+
+for(i=1;i<process.argv.length;i++){
+    inString+=process.argv[i];
+}
+
+/*console.log(process.argv);
+
+console.log(process.argv.length);
+
+console.log(inString);*/
+
+var clearString = inString.replace(/[^а-яА-ЯёЁa-zA-Z0-9]/g, "");
+
+//console.log(clearString);
+
+var reverseClearString =clearString.split('').reverse().join('');
 
 process.stdout.write(String(clearString.toUpperCase() === reverseClearString.toUpperCase()?'YES':'NO'));
