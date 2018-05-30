1e016c9ac4066c2a724cd67fbf61dfa569b7152f
Alena Zhilina <cont116@gmail.com>
2018-05-31 00:46:19 +0300

    task1.4

--- task1.js
+++ task1.js
@@ -4,15 +4,7 @@ for(i=1;i<process.argv.length;i++){
     inString+=process.argv[i];
 }
 
-/*console.log(process.argv);
-
-console.log(process.argv.length);
-
-console.log(inString);*/
-
 var clearString = inString.replace(/[^а-яА-ЯёЁa-zA-Z0-9]/g, "");
-
-//console.log(clearString);
 
 var reverseClearString =clearString.split('').reverse().join('');
 
